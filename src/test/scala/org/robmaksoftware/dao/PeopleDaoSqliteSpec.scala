c5d597414e1b01d2010f784b002fe7ab9e915aa6
robert.makyla <robert.makyla@gmail.com>
2023-08-10 23:43:32 +0200

    refactor specs

---
+++ src/test/scala/org/robmaksoftware/dao/PeopleDaoSqliteSpec.scala
@@ -0,0 +1,28 @@
+package org.robmaksoftware.dao
+
+import cats.effect.IO
+import org.robmaksoftware.domain.{Person, PersonId}
+import org.scalatest.{FutureOutcome, Outcome}
+
+class PeopleDaoSqliteSpec extends PeopleDaoSpec {
+
+  override def withFixture(test: OneArgAsyncTest): FutureOutcome = {
+    val ioOutcome: IO[Outcome] = Dao.dbDao[IO].use { dao: Dao[IO, PersonId, Person] =>
+      val testResult: FutureOutcome = withFixture(test.toNoArgAsyncTest(dao))
+      IO.fromFuture(IO(testResult.toFuture))
+    }
+    new FutureOutcome(ioOutcome.unsafeToFuture())
+  }
+
+
+  /*
+
+  // when Fixture in not wrapped in Resource :
+
+  override def withFixture(test: OneArgAsyncTest): FutureOutcome = {
+    val repo = Dao.inMemDao[IO]
+    new FutureOutcome(test(repo).toFuture)
+  }
+
+  */
+}
